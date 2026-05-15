9af14d69df437f0be2572e7e688ddce66dd81859
juansouza09 <juansouza9.dev@gmail.com>
2026-05-15 15:09:52 -0300

    fix(content): only center post image

--- site_unico/public_html/wp-content/mu-plugins/abiquifi-gutenberg-image-alignment.php
+++ site_unico/public_html/wp-content/mu-plugins/abiquifi-gutenberg-image-alignment.php
@@ -8,10 +8,20 @@ add_action(
 	'wp_enqueue_scripts',
 	static function () {
 		$css = '
+			.elementor-widget-raven-post-content {
+				text-align: left !important;
+			}
+
 			.elementor-widget-raven-post-content .wp-block-image figure.aligncenter,
 			.jupiterx-post-content .wp-block-image figure.aligncenter {
 				margin-left: auto !important;
 				margin-right: auto !important;
+				text-align: left !important;
+			}
+
+			.elementor-widget-raven-post-content .wp-block-image figcaption,
+			.jupiterx-post-content .wp-block-image figcaption {
+				text-align: left !important;
 			}
 		';
 
